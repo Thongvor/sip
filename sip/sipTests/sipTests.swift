e6a783322bf736cc7a2de6a41c65f2a0975cc552
D Holovko <dmtholovko@gmail.com>
2025-06-12 15:26:30 +0300

    init xcode project

---
+++ sip/sipTests/sipTests.swift
@@ -0,0 +1,17 @@
+//
+//  sipTests.swift
+//  sipTests
+//
+//  Created by Dmytro Holovko on 12.06.2025.
+//
+
+import Testing
+@testable import sip
+
+struct sipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
